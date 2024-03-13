659a915dfd678f9b0183d89252953c8be6f5f3dc
Sayantan Sadhu <dip.sadhu111@gmail.com>
2024-03-13 16:36:34 +0530

    work

--- chat_server/index.js
+++ chat_server/index.js
@@ -4,21 +4,19 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 var server = http.createServer(app);
-var io = require("socket.io")(server, {
-    cors:
-    {
-        origin: "*"
-    }
-});
+var io = require("socket.io")(server);
 
 
 app.use(express.json());
-app.use(cors());
 
-io.on("Connection", (socket) => {
+io.of('/test').on('connection', (socket) => {
     console.log("connected");
+    console.log(socket.id, "has joined");
+    socket.io("/test", (msg) => {
+        console.log(msg);
+    });
 });
 
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
     console.log("server started");
 })
